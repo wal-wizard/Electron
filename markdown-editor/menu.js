c9df30573c44fe13acf7b9dadd639c263eb46522
Waliston <wesantosdev@gmail.com>
2024-05-12 00:24:59 -0300

    menu itens update

--- markdown-editor/menu.js
+++ markdown-editor/menu.js
@@ -11,6 +11,15 @@ const template = [
                 }
             }
         ]
+    },
+    {
+        label: "Debugging",
+        submenu: [
+            {
+                label: "Dev Tools",
+                role: "toggleDevTools"
+            }
+        ]
     }
 ]
 
